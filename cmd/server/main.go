833ee1bdd211bea99ccd466273679e5f69b29a3d
Kyle Thomas <kyle@provide.services>
2021-06-12 18:28:05 -0400

    Start ssh server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"context"
-	"net/http"
+	"net"
 	"os"
 	"os/signal"
 	"sync/atomic"
 	"syscall"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/provideplatform/pgrok/common"
+	"golang.org/x/crypto/ssh"
 
-	provide "github.com/provideservices/provide-go/common"
 	util "github.com/provideservices/provide-go/common/util"
 )
 
@@ -25,7 +24,10 @@ var (
 	shutdownCtx context.Context
 	sigs        chan os.Signal
 
-	srv *http.Server
+	conn     net.Conn
+	server   *ssh.ServerConn
+	ingressc <-chan ssh.NewChannel
+	reqc     <-chan *ssh.Request
 )
 
 func init() {
@@ -36,6 +38,7 @@ func main() {
 	common.Log.Debugf("starting pgrok server...")
 	installSignalHandlers()
 
+	initTransport()
 	serveSSH()
 
 	timer := time.NewTicker(runloopTickInterval)
@@ -47,7 +50,7 @@ func main() {
 			// tick... no-op
 		case sig := <-sigs:
 			common.Log.Debugf("received signal: %s", sig)
-			srv.Shutdown(shutdownCtx)
+			server.Close()
 			shutdown()
 		case <-shutdownCtx.Done():
 			close(sigs)
@@ -74,15 +77,97 @@ func shutdown() {
 	}
 }
 
+func initTransport() {
+	// TODO:
+}
+
 func serveSSH() {
 	// TODO--
 	//	net/ssh package
+	var err error
+	server, ingressc, reqc, err = ssh.NewServerConn(conn, &ssh.ServerConfig{
+
+		// Rand provides the source of entropy for cryptographic
+		// primitives. If Rand is nil, the cryptographic random reader
+		// in package crypto/rand will be used.
+		// Rand io.Reader
+
+		// The maximum number of bytes sent or received after which a
+		// new key is negotiated. It must be at least 256. If
+		// unspecified, a size suitable for the chosen cipher is used.
+		// RekeyThreshold uint64
+
+		// The allowed key exchanges algorithms. If unspecified then a
+		// default set of algorithms is used.
+		// KeyExchanges []string
+
+		// The allowed cipher algorithms. If unspecified then a sensible
+		// default is used.
+		// Ciphers []string
+
+		// The allowed MAC algorithms. If unspecified then a sensible default
+		// is used.
+		// MACs []string
+
+		// NoClientAuth is true if clients are allowed to connect without
+		// authenticating.
+		// NoClientAuth bool
+
+		// MaxAuthTries specifies the maximum number of authentication attempts
+		// permitted per connection. If set to a negative number, the number of
+		// attempts are unlimited. If set to zero, the number of attempts are limited
+		// to 6.
+		// MaxAuthTries int
+
+		// PasswordCallback, if non-nil, is called when a user
+		// attempts to authenticate using a password.
+		// PasswordCallback func(conn ConnMetadata, password []byte) (*Permissions, error)
+
+		// PublicKeyCallback, if non-nil, is called when a client
+		// offers a public key for authentication. It must return a nil error
+		// if the given public key can be used to authenticate the
+		// given user. For example, see CertChecker.Authenticate. A
+		// call to this function does not guarantee that the key
+		// offered is in fact used to authenticate. To record any data
+		// depending on the public key, store it inside a
+		// Permissions.Extensions entry.
+		// PublicKeyCallback func(conn ConnMetadata, key PublicKey) (*Permissions, error)
+
+		// KeyboardInteractiveCallback, if non-nil, is called when
+		// keyboard-interactive authentication is selected (RFC
+		// 4256). The client object's Challenge function should be
+		// used to query the user. The callback may offer multiple
+		// Challenge rounds. To avoid information leaks, the client
+		// should be presented a challenge even if the user is
+		// unknown.
+		// KeyboardInteractiveCallback func(conn ConnMetadata, client KeyboardInteractiveChallenge) (*Permissions, error)
+
+		// AuthLogCallback, if non-nil, is called to log all authentication
+		// attempts.
+		// AuthLogCallback func(conn ConnMetadata, method string, err error)
+
+		// ServerVersion is the version identification string to announce in
+		// the public handshake.
+		// If empty, a reasonable default is used.
+		// Note that RFC 4253 section 4.2 requires that this string start with
+		// "SSH-2.0-".
+		// ServerVersion string
+
+		// BannerCallback, if present, is called and the return string is sent to
+		// the client after key exchange completed but before authentication.
+		// BannerCallback func(conn ConnMetadata) string
+
+		// GSSAPIWithMICConfig includes gssapi server and callback, which if both non-nil, is used
+		// when gssapi-with-mic authentication is selected (RFC 4462 section 3).
+		// GSSAPIWithMICConfig *GSSAPIWithMICConfig
+		// contains filtered or unexported fields
+	})
+
+	if err != nil {
+		common.Log.Panicf("failed to start pgrok server; %s", err.Error())
+	}
 
 	common.Log.Debugf("pgrok server listening on %s", util.ListenAddr)
-}
-
-func statusHandler(c *gin.Context) {
-	provide.Render(nil, 204, c)
 }
 
 func shuttingDown() bool {
--- go.mod
+++ go.mod
@@ -5,8 +5,9 @@ go 1.16
 replace github.com/provideapp/ident => ../ident
 
 require (
-	github.com/gin-gonic/gin v1.7.2
+	github.com/gin-gonic/gin v1.7.2 // indirect
 	github.com/kthomas/go-logger v0.0.0-20210526080020-a63672d0724c
 	github.com/provideapp/ident v0.0.0-00010101000000-000000000000
 	github.com/provideservices/provide-go v0.0.0-20210610091141-756e6385f6b8
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 )
