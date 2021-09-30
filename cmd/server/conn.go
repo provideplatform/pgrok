40f13b315284088f704cd896dea4e60aeb1be5cc
Kyle Thomas <kyle@provide.services>
2021-09-30 16:15:51 -0400

    Fix invalid reference to time.Day

--- cmd/server/conn.go
+++ cmd/server/conn.go
@@ -17,7 +17,7 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-const pgrokSubscriptionDefaultFreeTierTunnelDuration = time.Day * 7
+const pgrokSubscriptionDefaultFreeTierTunnelDuration = time.Hour * 24 * 7
 const pgrokSubscriptionDefaultCapacity = 0
 
 const sshChannelTypeForward = "forward"
